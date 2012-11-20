52e53dbe765537793f157f8c05f4394520aa2536
Mono <davido262@gmail.com>
2012-11-20 01:39:45 -0600

    player and ball movement implemented

--- main.cpp
+++ main.cpp
@@ -1,18 +1,93 @@
+/*
+ *    Permission is hereby granted, free of charge, to any person
+ *    obtaining a copy of this software and associated documentation
+ *    files (the "Software"), to deal in the Software without
+ *    restriction, including without limitation the rights to use,
+ *    copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the
+ *    Software is furnished to do so, subject to the following
+ *    conditions:
+ *
+ *    The above copyright notice and this permission notice shall be
+ *    included in all copies or substantial portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *    OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <cstdlib>
+#include <cmath>
+#include <ctime>
 #include <SDL.h>
 
 using namespace std;
 
+// constants
+const double PI = 3.1415926535897932384626433832795028841971693993751058;
+const double HALF_PI = PI * 0.5;
+const double QUARTER_PI = HALF_PI * 0.5;
+
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
+
 const int FRAMERATE_CAP = 60;
 const int MILLISECONDS_CAP = 1000 / FRAMERATE_CAP;
 
-void processEvents(bool& isRunning);
+const size_t VIRTUAL_SCREEN_PIXEL_SIZE = 4;
+const size_t VIRTUAL_SCREEN_WIDTH = WINDOW_WIDTH / VIRTUAL_SCREEN_PIXEL_SIZE;
+const size_t VIRTUAL_SCREEN_HEIGHT = WINDOW_HEIGHT / VIRTUAL_SCREEN_PIXEL_SIZE;
+
+const size_t BALL_RADIUS = VIRTUAL_SCREEN_WIDTH / 40;
+const size_t PLAYER_WIDTH = VIRTUAL_SCREEN_WIDTH / 40;
+const size_t PLAYER_HEIGHT = VIRTUAL_SCREEN_HEIGHT / 8;
+
+const double BALL_SPEED = 1.0;
+const double PLAYER_SPEED = 3.0;
+
+const size_t PLAYER1_POSITION_X = 8;
+const size_t PLAYER2_POSITION_X = VIRTUAL_SCREEN_WIDTH - PLAYER1_POSITION_X - PLAYER_WIDTH;
+
+const unsigned int BALL_COLOR =    0xaa2222;
+const unsigned int PLAYER1_COLOR = 0x22aa22;
+const unsigned int PLAYER2_COLOR = 0x2222aa;
 
 
 
+// function prototypes
+void processEventsSDL(bool& isRunning);
+void resetEverything();
+void update();
+void drawPixelSDL(const size_t posX, const size_t posY, const unsigned int color);
+void drawBall(const size_t posX, const size_t posY, const size_t radius, const unsigned int color);
+void drawRacket(const size_t posX, const size_t posY, const size_t w, const size_t h, const unsigned int color);
+void drawEverything();
+
+// global variables
+SDL_Surface* g_screen = 0;
+double g_player1Pos = (VIRTUAL_SCREEN_HEIGHT - PLAYER_HEIGHT) / 2;
+double g_player2Pos = g_player1Pos;
+double g_ballPosX = VIRTUAL_SCREEN_WIDTH / 2;
+double g_ballPosY = VIRTUAL_SCREEN_HEIGHT / 2;
+double g_ballAngle = 0.0;
+
+bool g_isKeyDownW = false;
+bool g_isKeyDownS = false;
+bool g_isKeyDownUP = false;
+bool g_isKeyDownDOWN = false;
+
+
+
+// main function
 int main(int argc, char** argv) {
     // introduction
     cout << "          *** Equipo 9 ***" << endl;
@@ -26,6 +101,7 @@ int main(int argc, char** argv) {
     cout << endl;
     cout << "Instrucciones:" << endl;
     cout << "[ESC]  Salir" << endl;
+    cout << "[r]    Reset" << endl;
     cout << "[w]    Mover jugador 1 arriba" << endl;
     cout << "[s]    Mover jugador 1 abajo" << endl;
     cout << "[UP]   Mover jugador 2 arriba" << endl;
@@ -37,15 +113,12 @@ int main(int argc, char** argv) {
     // initialization
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) // 0 success, -1 failure
         return EXIT_FAILURE;
-    SDL_Surface* screen = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-    if (screen == 0)
+    g_screen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+    if (g_screen == 0)
         return EXIT_FAILURE;
 
-    SDL_Rect rect;
-    rect.x = 100;
-    rect.y = 50;
-    rect.w = 4;
-    rect.h = 4;
+    srand(time(0));
+    resetEverything();
 
     // main loop
     Uint32 startTime;
@@ -55,16 +128,13 @@ int main(int argc, char** argv) {
         startTime = SDL_GetTicks();
 
         // update
-        processEvents(isRunning);
-        ++rect.x;
+        processEventsSDL(isRunning);
+        update();
 
         // draw
-        // TODO: implement assembly drawing routines
-        SDL_FillRect(screen, 0, 0); // clear screen
-
-        SDL_FillRect(screen, &rect, SDL_MapRGB(screen->format, 255, 0, 0));
-
-        SDL_Flip(screen); // flip buffers
+        SDL_FillRect(g_screen, 0, 0); // clear screen
+        drawEverything();
+        SDL_Flip(g_screen); // flip buffers
 
         // framerate cap
         deltaTime = SDL_GetTicks() - startTime;
@@ -85,7 +155,7 @@ int main(int argc, char** argv) {
 
 
 
-void processEvents(bool& isRunning) {
+void processEventsSDL(bool& isRunning) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
@@ -99,20 +169,146 @@ void processEvents(bool& isRunning) {
             case SDLK_ESCAPE:
                 isRunning = false;
                 break;
+            // reset
+            case SDLK_r:
+                resetEverything();
+                break;
 
             // player 1
             case SDLK_w:
+                g_isKeyDownW = true;
                 break;
             case SDLK_s:
+                g_isKeyDownS = true;
                 break;
 
             // player 2
             case SDLK_UP:
+                g_isKeyDownUP = true;
                 break;
             case SDLK_DOWN:
+                g_isKeyDownDOWN = true;
+                break;
+            }
+            break;
+
+        case SDL_KEYUP:
+            switch (event.key.keysym.sym) {
+            // player 1
+            case SDLK_w:
+                g_isKeyDownW = false;
+                break;
+            case SDLK_s:
+                g_isKeyDownS = false;
+                break;
+
+            // player 2
+            case SDLK_UP:
+                g_isKeyDownUP = false;
+                break;
+            case SDLK_DOWN:
+                g_isKeyDownDOWN = false;
                 break;
             }
             break;
         }
     }
+}
+
+void resetEverything() {
+    g_player1Pos = (VIRTUAL_SCREEN_HEIGHT - PLAYER_HEIGHT) / 2;
+    g_player2Pos = g_player1Pos;
+    g_ballPosX = VIRTUAL_SCREEN_WIDTH / 2;
+    g_ballPosY = VIRTUAL_SCREEN_HEIGHT / 2;
+    g_ballAngle = double(rand()) / double(RAND_MAX) * HALF_PI - QUARTER_PI;
+    if (rand() % 2 == 0)
+        g_ballAngle += PI;
+}
+
+void update() {
+    // update players
+    if (g_isKeyDownW) {
+        g_player1Pos -= PLAYER_SPEED;
+        if (g_player1Pos < 0.0)
+            g_player1Pos = 0.0;
+    }
+    if (g_isKeyDownS) {
+        g_player1Pos += PLAYER_SPEED;
+        if (g_player1Pos > VIRTUAL_SCREEN_HEIGHT - PLAYER_HEIGHT)
+            g_player1Pos = VIRTUAL_SCREEN_HEIGHT - PLAYER_HEIGHT;
+    }
+    if (g_isKeyDownUP) {
+        g_player2Pos -= PLAYER_SPEED;
+        if (g_player2Pos < 0.0)
+            g_player2Pos = 0.0;
+    }
+    if (g_isKeyDownDOWN) {
+        g_player2Pos += PLAYER_SPEED;
+        if (g_player2Pos > VIRTUAL_SCREEN_HEIGHT - PLAYER_HEIGHT)
+            g_player2Pos = VIRTUAL_SCREEN_HEIGHT - PLAYER_HEIGHT;
+    }
+
+    // update ball
+    g_ballPosX += BALL_SPEED * cos(g_ballAngle);
+    g_ballPosY -= BALL_SPEED * sin(g_ballAngle);
+
+    // check ball collisions with players
+
+    // check win conditions
+}
+
+void drawPixelSDL(const size_t posX, const size_t posY, const unsigned int color) {
+    SDL_Rect rect;
+    rect.x = posX * VIRTUAL_SCREEN_PIXEL_SIZE;
+    rect.y = posY * VIRTUAL_SCREEN_PIXEL_SIZE;
+    rect.w = VIRTUAL_SCREEN_PIXEL_SIZE;
+    rect.h = VIRTUAL_SCREEN_PIXEL_SIZE;
+    SDL_FillRect(g_screen, &rect, color);
+}
+
+void drawBall(const size_t posX, const size_t posY, const size_t radius, const unsigned int color) {
+    int x = 0;
+    int y = radius;
+    int p = 1 - radius;
+
+    while(x <= y) {
+        drawPixelSDL(posX + x, posY + y, color);
+        drawPixelSDL(posX + x, posY - y, color);
+        drawPixelSDL(posX - x, posY + y, color);
+        drawPixelSDL(posX - x, posY - y, color);
+        drawPixelSDL(posX + y, posY + x, color);
+        drawPixelSDL(posX + y, posY - x, color);
+        drawPixelSDL(posX - y, posY + x, color);
+        drawPixelSDL(posX - y, posY - x, color);
+
+        if(p < 0) {
+            ++x;
+            p += 2 * x + 1;
+        }
+        else {
+            ++x;
+            --y;
+            p += 2 * (x - y) + 1;
+        }
+    }
+}
+
+void drawRacket(const size_t posX, const size_t posY, const size_t w, const size_t h, const unsigned int color) {
+    size_t x, y;
+    size_t maxX = posX + w - 1;
+    size_t maxY = posY + h - 1;
+    for (x = posX; x <= maxX; ++x) {
+        drawPixelSDL(x, posY, color);
+        drawPixelSDL(x, maxY, color);
+    }
+    for (y = posY + 1; y <= maxY; ++y) {
+        drawPixelSDL(posX, y, color);
+        drawPixelSDL(maxX, y, color);
+    }
+}
+
+void drawEverything() {
+    drawRacket(PLAYER1_POSITION_X, g_player1Pos, PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER1_COLOR);
+    drawRacket(PLAYER2_POSITION_X, g_player2Pos, PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER2_COLOR);
+    drawBall(g_ballPosX, g_ballPosY, BALL_RADIUS, BALL_COLOR);
 }
